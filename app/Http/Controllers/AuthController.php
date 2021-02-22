f75928bcbec435527fd992786ee8b93d198f3d3e
Aleksandar <90stepeni@gmail.com>
2021-02-22 20:08:45 +0100

    add functional auth controller with coresponding api routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $registerData = $request->validated();
+        $registerData['password'] = Hash::make($registerData['password']);
+        $user = User::create($registerData);
+        $token = Auth::login($user);
+
+        return $this->jsonResponseWithToken($token);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        $token = Auth::attempt($credentials);
+        
+        return $this->jsonResponseWithToken($token);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'logout' => true
+        ]);
+    }
+
+    //helper methodes
+    private function jsonResponseWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user()
+        ]);
+    }
+}
